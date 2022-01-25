51c4f4c5d27b9420edd0b818b856f2ff9b44c8e8
Prazepam <vincentbriquet237@gmail.com>
2022-01-25 16:06:47 +0100

    ajout validations/error
    ajout des validations ou messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,99 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+let form = document.getElementById("inscription");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+  let location = document.getElementById("location");
+  let conditions = document.getElementById("check-condition");
+  
+  
+  const numRegExp = /^[0-9]+$/;
+  validFirst();
+  validLast();
+  validEmail();
+  validBirthdate();
+  validTournament();
+  validCondition();
+ 
+
+});
+//Prénom//
+ function validFirst(){
+  const nameRegExp = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+  let inputfirst = document.getElementById("first");
+  if (nameRegExp.test(inputfirst.value)){
+    first.parentElement.setAttribute('data-error-visible', 'false');
+  }
+  else{
+    first.parentElement.setAttribute('data-error-visible', 'true');
+  }
+ }
+
+//Nom//
+ function validLast() {
+  const nameRegExp = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+  let inputlast = document.getElementById("last");
+  if (nameRegExp.test(inputlast.value)){
+    last.parentElement.setAttribute('data-error-visible', 'false')
+  }
+  else{
+    last.parentElement.setAttribute('data-error-visible', 'true')
+  }
+ };
+
+ //Email//
+ function validEmail() {
+  const emailRegExp = /^[a-zA-A0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+  let inputemail = document.getElementById("email");
+  if (emailRegExp.test(inputemail.value)){
+    email.parentElement.setAttribute('data-error-visible', 'false')
+  }
+  else{
+    email.parentElement.setAttribute('data-error-visible', 'true')
+  }
+ };
+
+ //Date de naissance//
+ function validBirthdate(){
+  let inputbirthdate = document.getElementById("birthdate");
+  if (inputbirthdate.value){
+    birthdate.parentElement.setAttribute('data-error-visible', 'false');
+  }
+  else{
+    birthdate.parentElement.setAttribute('data-error-visible', 'true');
+  }
+ }
+
+ //Tournois//
+ function validTournament(){
+   let inputtournament = document.getElementsByClassName("checkbox-input"), i, count = 0;
+   for (let i = 0; i < inputtournament.length; i++){
+    if (inputtournament.checked == true){
+     location.parentElement.setAttribute('data-error-visible', 'false');
+    }
+    else{
+     location.parentElement.setAttribute('data-error-visible', 'true');
+    }
+  }
+ }
+
+ //Condition//
+ function checkMe() {
+   let inputCondition =document.getElementById("checkbox1");
+   if (inputCondition.checked == true){
+     checkbox1.parentElement.setAttribute('data-error-visible', 'false');
+   }
+   else{
+     checkbox1.parentElement.setAttribute('data-error-visible', 'true');
+   }
+ };
